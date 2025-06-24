5e4a86a77f29a380e6c45ad62e15bb516eb77fa1
txoxu <king-crimson05@outlook.jp>
2025-06-24 11:15:24 +0900

    [fix]controller側にclickイベントを定義、view側ではpublicでbutton,

--- WinFormsTask1/Form1.vb
+++ WinFormsTask1/Form1.vb
@@ -1,9 +1,12 @@
 ﻿Public Class Form1
+
     '課題１ 文字列処理の基本操作ツールを作ろう
-    Private Sub btnProcess_Click(sender As Object, e As EventArgs) Handles btnProcess.Click
-        lstOutput.Items.Clear()
 
-        lstOutput.Items.AddRange(Task1Controller.ProcessAll(txtInput.Text).ToArray())
+    Public btnProcess As Button
+    Public txtInput As TextBox
+    Public lstOutput As ListBox
 
+    Private Sub Form1_load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Task1Controller.Initialize(Me)
     End Sub
 End Class
